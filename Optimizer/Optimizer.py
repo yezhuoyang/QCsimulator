d9a79ee36eb73ef39752d2b14c5a1146cb7786b5
Zhuoyang Ye <yezhuoyang98@gmail.com>
2023-11-04 17:22:24 -0700

    Layeralign class and single gate merge class

--- Optimizer/Optimizer.py
+++ Optimizer/Optimizer.py
@@ -5,6 +5,7 @@ from typing import List, Union, Any
 
 '''
 Circuit Optimizer Class
+The optimizer can only change the circuit.calc_sequence
 '''
 
 
@@ -19,4 +20,52 @@ class CircuitOptimizer:
         raise NotImplementedError("Subclasses must implement optimize method.")
 
 
+'''
+Align all gates into different columns.
+Fill the column as much as possible.
+For example, the input circuit sequence of length 9 looks like:
+        ---H---X---Y---------------
+        --------------Z-T------H---
+        -H-----------------Tdg-----H
+After optimization, it should look like:
+        ---H---X---Y---
+        ---Z---T---H---
+        ---H---Tdg-H---   
+Now, the input circuit sequence only has length 3. This reduce the time to do
+kroneck product        
+'''
 
+
+class LayerAlign(CircuitOptimizer):
+    def __init__(self, num_qubits: int) -> None:
+        self.num_qubits = num_qubits
+
+    def set_circuit(self, circuit: QuantumCircuit) -> None:
+        self.circuit = circuit
+
+    def optimize(self) -> NotImplementedError:
+        raise NotImplementedError("Subclasses must implement optimize method.")
+
+
+'''
+Merge all single qubit gates next to each other as much as possible 
+For example, the input circuit sequence of length 9 looks like:
+        ---H---X---Y---------------
+        --------------Z-T------H---
+        -H-----------------Tdg-----H
+After optimization, it should look like:
+        ---G1=H*X*Y---
+        ---G2Z*T*H---
+        ---G3=H*Tdg*H---   
+'''
+
+
+class SingleGateMerge(CircuitOptimizer):
+    def __init__(self, num_qubits: int) -> None:
+        self.num_qubits = num_qubits
+
+    def set_circuit(self, circuit: QuantumCircuit) -> None:
+        self.circuit = circuit
+
+    def optimize(self) -> NotImplementedError:
+        raise NotImplementedError("Subclasses must implement optimize method.")
