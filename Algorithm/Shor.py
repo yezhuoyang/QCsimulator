9d8cbe8fccde5eb55bba6b981bce5b73d80a3f8c
Zhuoyang ye <737475845@qq.com>
2023-11-08 16:15:22 -0800

    Shor's classical part.

--- Algorithm/Shor.py
+++ Algorithm/Shor.py
@@ -11,12 +11,38 @@ class Shor(QuantumAlgorithm):
     def __init__(self, num_qubits: int) -> None:
         super().__init__(num_qubits)
         self.num_qubits = num_qubits
+        self._N=0
+
 
     def construct_circuit(self) -> NotImplementedError:
         raise NotImplementedError("Subclasses must implement construct_circuit method.")
 
-    def set_input(self, alginput: List) -> NotImplementedError:
-        raise NotImplementedError("Subclasses must implement set_input method.")
+    def set_input(self, N:int) -> None:
+        self._N=N
+        
 
-    def compute_result(self) -> NotImplementedError:
-        raise NotImplementedError("Subclasses must implement compute_result method.")
+
+    '''
+    Use classical algorithm to find the order of a.
+    The result is the order r, such that a^r mod N = 1
+    '''
+    def order_finding_classical(self,a:int) -> int:
+        
+        return
+
+    '''
+    In this implmentation, we only need to return 1 non trivial factor of N
+    '''
+    def compute_result(self) -> None:
+        '''
+        First determine if N is a even number
+        '''
+        if self._N%2==0:
+            self.solution=2
+            return
+        
+        
+        
+        
+        
+        
