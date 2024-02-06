d0bb60fd1773562a979d2168020b6aae40f6a551
Zhuoyang Ye <yezhuoyang98@gmail.com>
2024-02-06 11:29:42 -0800

    [File] Add BlockEncoding, HamiltonianSimulation and PhaseAmplification

---
+++ Algorithm/PhaseAmplification.py
@@ -0,0 +1,28 @@
+import qiskit
+import functools
+from qiskit_aer import AerSimulator
+from Algorithm import QuantumAlgorithm
+import Gate
+import Parameter
+import Circuit
+from typing import List, Union, Any
+
+
+
+
+
+
+
+class PhaseAmplification(QuantumAlgorithm):
+    def __init__(self, num_qubits: int) -> None:
+        super().__init__(num_qubits)
+        self.num_qubits = num_qubits
+
+    def construct_circuit(self) -> NotImplementedError:
+        raise NotImplementedError("Subclasses must implement construct_circuit method.")
+
+    def set_input(self, alginput: List) -> NotImplementedError:
+        raise NotImplementedError("Subclasses must implement set_input method.")
+
+    def compute_result(self) -> NotImplementedError:
+        raise NotImplementedError("Subclasses must implement compute_result method.")
