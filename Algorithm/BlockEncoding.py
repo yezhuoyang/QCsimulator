825374d563665558e92a7dcc9e4abc1a42b64968
Zhuoyang Ye <yezhuoyang98@gmail.com>
2024-02-14 02:14:42 -0800

    [Feat] Successfully reproduce BCM3, BCM4 in LinLin's block encoding paper.

--- Algorithm/BlockEncoding.py
+++ Algorithm/BlockEncoding.py
@@ -7,7 +7,14 @@ import Circuit
 from typing import List, Union, Any
 import qiskit
 import qiskit.quantum_info as qi
-from qiskit.circuit.library.standard_gates import XGate
+from qiskit.circuit.library.standard_gates import XGate, RYGate
+
+
+
+
+def norm_2_distance(matrix1: np.ndarray, matrix2: np.ndarray):
+    return np.sum((matrix1 - matrix2) ** 2)
+
 
 '''
 The block encoding class for BCM matrix
@@ -18,26 +25,29 @@ class BlockEncoding_qiskit(QuantumAlgorithm):
     def __init__(self, m: int, n: int):
         self.num_qubits = m + n + 1
         self._n = n
-        self._m = m
+        self._m = 2
         super().__init__(self.num_qubits)
         self.circuit = qiskit.QuantumCircuit(self.num_qubits, m + 1)
-
+        self._constructed = False
 
     '''
     The input of the BCM block encoding is alpha,beta,gamma
     '''
 
-    def set_input(self, alpha: float, beta: float, gamma: float):
-        self._alpha = alpha
-        self._beta = beta
-        self._gamma = gamma
+    def set_input(self, input: List):
+        self._alpha = input[0]
+        self._beta = input[1]
+        self._gamma = input[2]
 
     def construct_circuit(self):
+        if self._constructed:
+            return
         self.construct_diffusion()
         self.construct_OA()
         self.construct_Oc()
         self.construct_diffusion()
-        self.circuit.measure(list(range(0, self._m + 1)), list(range(0, self._m + 1)))
+        # self.circuit.measure(list(range(self._n, self._n + self._m + 1)), list(range(0, self._m + 1)))
+        self._constructed = True
         return
 
     '''
@@ -47,37 +57,52 @@ class BlockEncoding_qiskit(QuantumAlgorithm):
 
     def get_encoded_matrix(self):
         op = qi.Operator(self.circuit)
-        return np.array(op)[0:2 ** self._n, 0:2 ** self._n]
+        return np.real(np.array(op)[0:2 ** self._n, 0:2 ** self._n])
 
     def construct_OA(self):
-        return
+        theta0 = 2 * np.arccos(self._gamma)
+        rot0 = RYGate(theta0).control(2, ctrl_state='00')
+        self.circuit.append(rot0, [self._n + self._m - 1, self._n + self._m - 2, self._n + self._m])
+        theta1 = 2 * np.arccos(self._alpha - 1)
+        rot1 = RYGate(theta1).control(2, ctrl_state='01')
+        self.circuit.append(rot1, [self._n + self._m - 1, self._n + self._m - 2, self._n + self._m])
+        theta2 = 2 * np.arccos(self._beta)
+        rot2 = RYGate(theta2).control(2, ctrl_state='10')
+        self.circuit.append(rot2, [self._n + self._m - 1, self._n + self._m - 2, self._n + self._m])
 
     def construct_Oc(self):
+        circuit_L = self.get_L_circuit()
+        circuit_R = self.get_R_circuit()
+        control_indices = [self._n + 1, self._n]
+        gate_indices = list(range(0, self._n))
+        gate_L = circuit_L.to_gate().control(2, ctrl_state='10')
+        gate_R = circuit_R.to_gate().control(2, ctrl_state='00')
+        self.circuit.append(gate_R, control_indices + gate_indices)
+        self.circuit.append(gate_L, control_indices + gate_indices)
         return
 
     def construct_diffusion(self):
-        self.circuit.h(list(range(1, self._m + 1)))
+        self.circuit.h(list(range(self._n, self._n + self._m)))
 
-    def construct_L(self):
+    def get_L_circuit(self):
         circuit_L = qiskit.QuantumCircuit(self._n)
         for index in range(0, self._n - 1):
             control_qubits = list(range(0, self._n - 1 - index))
             target_qubit = self._n - 1 - index
-
             circuit_L.mcx(control_qubits,
-                             target_qubit=[target_qubit])
+                          target_qubit=[target_qubit])
         circuit_L.x(0)
         return circuit_L
 
-    def construct_R(self):
+    def get_R_circuit(self):
         circuit_R = qiskit.QuantumCircuit(self._n)
         for index in range(0, self._n - 1):
             num_control = self._n - 1 - index
             control_qubits = list(range(0, num_control))
             target_qubit = self._n - 1 - index
 
-            cccx = XGate().control(num_ctrl_qubits=self._n - 1 - index, ctrl_state='0'*num_control)
-            circuit_R.append(cccx,control_qubits+[target_qubit])
+            cccx = XGate().control(num_ctrl_qubits=self._n - 1 - index, ctrl_state='0' * num_control)
+            circuit_R.append(cccx, control_qubits + [target_qubit])
         circuit_R.x(0)
         return circuit_R
 
@@ -88,21 +113,22 @@ class BlockEncoding_qiskit(QuantumAlgorithm):
         )
         np.fill_diagonal(A, self._alpha)
         for i in range(size - 1):
-            A[i, i + 1] = self._beta
-            A[i + 1, i] = self._gamma
-        A[0, size - 1] = self._gamma
-        A[size - 1, 0] = self._beta
-        return A
+            A[i, i + 1] = self._gamma
+            A[i + 1, i] = self._beta
+        A[0, size - 1] = self._beta
+        A[size - 1, 0] = self._gamma
+        return A / (2 ** self._m)
 
     def compute_result(self):
-        return
+        self.construct_circuit()
+        return self.get_encoded_matrix()
 
     '''
     Return the accuracy for block encoding
     '''
 
     def accuracy(self):
-        return 0
+        return 1-norm_2_distance(self.compute_result(),self.get_BCM())
 
 
 class BlockEncoding(QuantumAlgorithm):
@@ -121,14 +147,27 @@ class BlockEncoding(QuantumAlgorithm):
 
 
 if __name__ == "__main__":
-    BC = BlockEncoding_qiskit(3, 2)
+    BCM3 = BlockEncoding_qiskit(2, 3)
+    BCM3.set_input([0.2, 0.3, 0.4])
+    print("------------The target matrix----------------------------")
+    print(BCM3.get_BCM())
+    final_matrix_BC3 = BCM3.compute_result()
+    print("--------------The BCM3(0.2,0.3,0.4) result--------------------------")
+    print(final_matrix_BC3)
+    print("--------------Fidelity calculated by matrix 2 norm-------------------")
+    print(BCM3.accuracy())
+    print("---------------The end-------------------------")
 
-    circuit_L=BC.construct_L()
-    circuit_R=BC.construct_R()
-    op = qi.Operator(circuit_L)
-    print(np.array(op))
 
-    circuit_R=BC.construct_R()
-    op = qi.Operator(circuit_R)
-    print(np.array(op))
 
+
+    BC4 = BlockEncoding_qiskit(2, 4)
+    BC4.set_input([0.2, 0.3, 0.4])
+    print("------------The target matrix----------------------------")
+    print(BC4.get_BCM())
+    final_matrix_BC4 = BC4.compute_result()
+    print("--------------The BCM4(0.2,0.3,0.4) result--------------------------")
+    print(final_matrix_BC4)
+    print("--------------Fidelity calculated by matrix 2 norm-------------------")
+    print(BC4.accuracy())
+    print("---------------The end-------------------------")
